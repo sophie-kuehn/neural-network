2832809f40c5fb084d83fed3c626f050187ec2c6
sophie <sophie@myhotelshop.de>
2023-01-26 20:57:01 +0100

    ...

--- main.cpp
+++ main.cpp
@@ -28,7 +28,6 @@ namespace SNN
     public:
         Neuron* inputNeuron;
         Neuron* outputNeuron;
-
         double weight = 0;
 
         double getValue();
@@ -40,12 +39,35 @@ namespace SNN
         std::vector<Synapse*> inputSynapses;
         std::vector<Synapse*> outputSynapses;
         ActivationFunction* activationFunction = nullptr;
-
+        bool isBias = false;
+        bool cacheValue = false;
         double value = 0;
+        double smallDelta = 0;
+
+        bool isInput()
+        {
+            return (this->inputSynapses.size() == 0);
+        };
+
+        bool isOutput()
+        {
+            return (this->outputSynapses.size() == 0);
+        };
+
+        void reset()
+        {
+            this->value = 0;
+            this->smallDelta = 0;
+            this->cacheValue = false;
+        };
 
         double getValue()
         {
-            double value = this->value;
+            if (this->isBias) return 1;
+            if (this->isInput() || this->cacheValue) return this->value;
+
+            double value = 0;
+
             for (const auto& synapse : this->inputSynapses) {
                 value += synapse->getValue();
             }
@@ -54,7 +76,27 @@ namespace SNN
                 value = activationFunction->activate(value);
             }
 
-            return value;
+            this->cacheValue = true;
+            return this->value = value;
+        };
+
+        void learn(double expectedValue, double epsilon = 0.01)
+        {
+            if (this->isInput()) return;
+
+            if (this->isOutput()) {
+                this->smallDelta = expectedValue - this->getValue();
+
+            } else {
+                for (const auto& synapse : this->outputSynapses) {
+                    this->smallDelta += synapse->outputNeuron->smallDelta * synapse->weight;
+                }
+            }
+
+            for (const auto& synapse : this->inputSynapses) {
+                double bigDelta = epsilon * this->smallDelta * synapse->inputNeuron->getValue();
+                synapse->weight += bigDelta;
+            }
         };
     };
 
@@ -71,72 +113,89 @@ namespace SNN
     public:
         std::vector<NeuronLayer> neurons;
 
-        Neuron* addNeuron(ActivationFunction* activationFunction)
-        {
-            return this->addNeuron(0.0, activationFunction);
-        };
-
-        Neuron* addNeuron(double value = 0.0, ActivationFunction* activationFunction = nullptr)
+        Neuron* addNeuron(ActivationFunction* activationFunction = nullptr)
         {
             auto neuron = new Neuron;
-            neuron->value = value;
             neuron->activationFunction = activationFunction;
             this->neurons.back().push_back(neuron);
             return neuron;
         };
 
-
         void openNewLayer()
         {
+            // add bias
+            if (this->neurons.size() > 0) {
+                auto neuron = new Neuron;
+                neuron->isBias = true;
+                this->neurons.back().push_back(neuron);
+            }
+
             NeuronLayer layer;
             this->neurons.push_back(layer);
         };
 
+        void addLayer(int numberOfNeurons = 1, ActivationFunction* activationFunction = nullptr)
+        {
+            this->openNewLayer();
+            for (int i = 0; i < 10; i++) this->addNeuron(activationFunction);
+        };
+
         void createSynapses()
         {
+            srand(time(0));
             int leftLayer = 0;
             for (const auto& leftNeurons : this->neurons) {
-                this->createSynapsesBetween(leftLayer, leftLayer+1);
+                int rightLayer = leftLayer+1;
+                if (this->neurons.size()-1 < rightLayer) return;
+                for (const auto& leftNeuron : leftNeurons) {
+                    for (const auto& rightNeuron : this->neurons[rightLayer]) {
+                        if (rightNeuron->isBias) continue;
+                        auto synapse = new Synapse;
+                        leftNeuron->outputSynapses.push_back(synapse);
+                        rightNeuron->inputSynapses.push_back(synapse);
+                        synapse->inputNeuron = leftNeuron;
+                        synapse->outputNeuron = rightNeuron;
+                        synapse->weight = (rand() % 100) / 100.0;
+                    }
+                }
                 leftLayer++;
             }
         };
 
-        void createSynapsesBetween(int leftLayer, int rightLayer)
+        DoubleVector process(DoubleVector input, DoubleVector expectedOutput = {}, double epsilon = 0.01)
         {
-            if (this->neurons.size()-1 < rightLayer) return;
-
-            for (const auto& leftNeuron : this->neurons[leftLayer]) {
-                for (const auto& rightNeuron : this->neurons[rightLayer]) {
-                    auto synapse = new Synapse;
-                    leftNeuron->outputSynapses.push_back(synapse);
-                    rightNeuron->inputSynapses.push_back(synapse);
-                    synapse->inputNeuron = leftNeuron;
-                    synapse->outputNeuron = rightNeuron;
+            for (const auto& neurons : this->neurons) {
+                for (const auto& neuron : neurons) {
+                    neuron->reset();
                 }
             }
 
-        };
-
-        void randomizeWeights()
-        {
-            srand(time(0));
-
-            for (const auto& layer : this->neurons) {
-                for (const auto& neuron : layer) {
-                    for (const auto& synapse : neuron->outputSynapses) {
-                        synapse->weight = (rand() % 201) / 100.0 - 1.0;
-                    }
-                }
+            int index = 0;
+            for (const auto& neuron : this->neurons[0]) {
+                double value = 0;
+                if (input.size() > index) value = input[index];
+                neuron->value = value;
+                index++;
             }
-        };
 
-        DoubleVector getOutput()
-        {
-            DoubleVector result;
+            DoubleVector output;
             for (const auto& neuron : this->neurons.back()) {
-                result.push_back(neuron->getValue());
+                output.push_back(neuron->getValue());
             }
-            return result;
+
+            if (expectedOutput.size() == 0) return output;
+
+            for(int i = this->neurons.size()-1; i >= 0; i--) {
+                index = 0;
+                for (const auto& neuron : this->neurons[i]) {
+                    double expectedOutputValue = 0;
+                    if (expectedOutput.size() > index) expectedOutputValue = expectedOutput[index];
+                    neuron->learn(expectedOutputValue, epsilon);
+                    index++;
+                }
+            }
+
+            return output;
         };
     };
 };
@@ -144,30 +203,33 @@ namespace SNN
 
 int main(int argc, char **argv)
 {
+    // nn --network="3;10,i;10,i;1" --input="10;10;10" --epsilon=0.00005 --iterations=50000
+
+
+    // SETUP
+
     auto network = new SNN::Network;
-
     auto actFn = new SNN::IdentityFunction;
-
-    network->openNewLayer();
-    network->addNeuron(1);
-    network->addNeuron(2);
-    network->addNeuron(3);
-    network->addNeuron(1); // bias
-
-    network->openNewLayer();
-    for (int i = 0; i < 10; i++) network->addNeuron(actFn);
-    network->openNewLayer();
-    for (int i = 0; i < 10; i++) network->addNeuron(actFn);
-    network->openNewLayer();
-    network->addNeuron();
+    network->addLayer(3);
+    network->addLayer(10, actFn);
+    network->addLayer(10, actFn);
+    network->addLayer(1);
     network->createSynapses();
-    network->randomizeWeights();
 
-    double expectedResult = 1.0 + 2.0 + 3.0;
-    SNN::DoubleVector result = network->getOutput();
-    double diff = fabs(result[0] - expectedResult);
+    // TESTRUN
 
-    std::cout << std::to_string(result[0]) << " - diff: " << std::to_string(diff) << std::endl;
+    for (int i=0; i<50000; i++) {
+        SNN::DoubleVector input = {
+            (rand() % 10),
+            (rand() % 10),
+            (rand() % 10)
+        };
+
+        SNN::DoubleVector expected = {input[0] + input[1] + input[2]};
+        SNN::DoubleVector output = network->process(input, expected, 0.00005);
+
+        std::cout << std::to_string(expected[0]) << " - op: " << std::to_string(output[0]) << " - diff: " << std::to_string(fabs(output[0] - expected[0])) << std::endl;
+    }
 
     return 0;
 }
